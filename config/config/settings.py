2ee8f7b4150659308f3f4799a706dccce406b18a
soniaBebebe <sonctonc@gmail.com>
2025-11-23 21:46:22 -0800

    bebebe

--- config/config/settings.py
+++ config/config/settings.py
@@ -130,3 +130,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+
+EMAIL_HOST_USER="sonctonc@gmail.com"
+EMAIL_HOST_PASSWORD="vdoi winy rdqa bkcx"
+DEFAULT_FROM_EMAIL=EMAIL_HOST_USER
